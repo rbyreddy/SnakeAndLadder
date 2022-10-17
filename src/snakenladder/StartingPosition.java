f6d4b45a7fdefc56d605755cfdab5e444aef62b1
Rakesh <byreddyrakesh41@gmail.com>
2022-10-17 20:17:55 +0530

    ADDED: Code for Player at Starting Position O

--- src/snakenladder/StartingPosition.java
+++ src/snakenladder/StartingPosition.java
@@ -1,4 +1,11 @@
 package snakenladder;
 
 public class StartingPosition {
-}
+    public static void main(String[] args) {
+            int pos = 0;
+            System.out.println("Welcome to Snake And Ladder program");
+            System.out.println("Single player at start position "+ pos);
+
+        }
+
+    }
